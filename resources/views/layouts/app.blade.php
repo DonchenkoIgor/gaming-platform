62ca5f600b011affb8b9be507bd1b5cfe3c677a9
DonchenkoIgor <idonchenko25@gmail.com>
2025-07-08 23:19:01 +0300

    images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,5 +69,62 @@
         <img src="{{asset('img/banner.png')}}" alt="">
     </div>
 </div>
+
+<div class="features">
+    <div class="container">
+        <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        <div class="info">
+            <div class="block">
+                <img src="{{asset('img/f1.png')}}" alt="">
+                <p>Mobile Game Development</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+            <div class="block">
+                <img src="{{asset('img/f2.png')}}" alt="">
+                <p>PC Game Development</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+            <div class="block">
+                <img src="{{asset('img/f3.png')}}" alt="">
+                <p>PS4 Game Development</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+            <div class="block">
+                <img src="{{asset('img/f4.png')}}" alt="">
+                <p>AR/VR Solutions</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+            <div class="block">
+                <img src="{{asset('img/f5.png')}}" alt="">
+                <p>AR/ VR design</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+            <div class="block">
+                <img src="{{asset('img/f6.png')}}" alt="">
+                <p>3D Modelings</p>
+                <img src="{{asset('img/arrow.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="wrapper">
+    <div class="container projects">
+        <h3>Our Recent Projects</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <div class="images">
+            <img src="{{asset('img/i1.png')}}" alt="">
+            <img src="{{asset('img/i2.png')}}" alt="">
+            <img src="{{asset('img/i3.png')}}" alt="">
+        </div>
+        <div class="images">
+            <img src="{{asset('img/i4.png')}}" alt="">
+            <img src="{{asset('img/i5.png')}}" alt="">
+            <img src="{{asset('img/i6.png')}}" alt="">
+        </div>
+        <a href="" class="see-all">SEE ALL</a>
+    </div>
+</div>
 </body>
 </html>
